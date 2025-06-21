2bae2e694ebcc3ae007cc0950819df1ece385f56
Jetpack Crow <anthelion717@gmail.com>
2025-06-21 17:53:10 +0000

    Tweaks to index.mjs and about.json to allow pattern information to display properly

--- designs/jett/src/index.mjs
+++ designs/jett/src/index.mjs
@@ -1,5 +1,5 @@
 import { Design, mergeI18n } from '@freesewing/core'
-import { data } from '../data.mjs'
+import about from '../about.json' with { type: 'json' }
 
 //import { i18n as brianI18n } from '@freesewing/brian'
 
@@ -22,7 +22,7 @@ import { cuff } from './cuff.mjs'
 
 // Create new design
 const Jett = new Design({
-  data,
+  data: about,
   parts: [
     front,
     back,
