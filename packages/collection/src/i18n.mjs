60ad5359c3fd7232f0bd1b9e2c9fb26b79e2cbce
Timur Duehr <tduehr@gmail.com>
2025-08-16 14:04:33 +0200

    [sunny] feat: Sunny the split side skirt (#501)
    Split side skirt in Victorian style
    
    Reviewed-on: https://codeberg.org/freesewing/freesewing/pulls/501
    Reviewed-by: Joost De Cock <joostdecock@noreply.codeberg.org>
    Co-authored-by: Timur Duehr <tduehr@gmail.com>
    Co-committed-by: Timur Duehr <tduehr@gmail.com>

--- packages/collection/src/i18n.mjs
+++ packages/collection/src/i18n.mjs
@@ -47,6 +47,7 @@ import { i18n as shin } from '@freesewing/shin'
 import { i18n as simon } from '@freesewing/simon'
 import { i18n as simone } from '@freesewing/simone'
 import { i18n as skully } from '@freesewing/skully'
+import { i18n as sunny } from '@freesewing/sunny'
 import { i18n as sven } from '@freesewing/sven'
 import { i18n as tamiko } from '@freesewing/tamiko'
 import { i18n as teagan } from '@freesewing/teagan'
@@ -113,6 +114,7 @@ export const i18n = {
   simon,
   simone,
   skully,
+  sunny,
   sven,
   tamiko,
   teagan,
